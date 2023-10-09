68f8a70863682497cf2d6b9e92dd18e237202983
dittmanndiogo <dittmanndiogo1996@gmail.com>
2023-10-09 10:11:04 -0300

    finished API functions

---
+++ src/router/index.ts
@@ -0,0 +1,14 @@
+import { Router } from "express"
+
+import authentication from "./authentication";
+import users from "./users";
+
+const router = Router()
+
+export default (): Router => {
+
+    authentication(router);
+    users(router);
+
+    return router;
+}
